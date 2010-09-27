99dcdb469b0be71a9ac7f0204a918d15fedbb5bc
Simon Larsen <simonhffh@gmail.com>
2010-09-27 15:04:34 +0200

    first commit

---
+++ Shape.java
@@ -0,0 +1,25 @@
+public class Shape{
+	public Shape{
+	}
+
+	boolean intersect(Vector3f vec){
+		return false;
+	}
+
+	boolean checkBBox(Vector3f vec){
+		return false;
+	}
+}
+
+public class Sphere extends Shape{
+	private float a,b,c,r;
+
+	public Sphere(float a, float b, float c, float r){
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.r = r;
+	}
+
+}
+
---
+++ Vector3f.java
@@ -0,0 +1,40 @@
+public class Vector3f{
+	public float x,y,z;
+
+	public Vector3f(float x, float y, float z){
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public Vector3f(){
+		x = 0.f;
+		y = 0.f;
+		z = 0.f;
+	}
+
+	public float length(){
+		return (float)Math.sqrt(x*x + y*y + z*z);
+	}
+
+	public Vector3f normalize(){
+		float l = length();
+		return new Vector3f(x/l, y/l, z/l);	
+	}
+
+	public static Vector3f crossp(Vector3f a, Vector3f b){
+		return new Vector3f(a.y*b.z-a.z*b.y, a.z*b.x-a.x*b.z, a.x*b.y-a.y*b.x); 
+	}
+
+	public String toString(){
+		return new String("[ " + x + " , " + y + " , " + z + " ]");
+	}
+
+	public Vector3f add(Vector3f i){
+		return new Vector3f(x+i.x, y+i.y, z+i.z);
+	}
+
+	public Vector3f sub(Vector3f i){
+		return new Vector3f(x-i.x, y-i.y, z-i.z);
+	}
+}
